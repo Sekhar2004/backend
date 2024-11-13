622cbcb03c20d82f42d02c92e0204edc4c033e1e
Sekhar2004 <jeerurajasekharreddy@gmail.com>
2024-11-13 23:13:52 +0530

    modified

--- server.js
+++ server.js
@@ -3,28 +3,22 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 require('dotenv').config();
 
-// Initialize express
 const app = express();
 
-// Connect to database
 connectDB();
 
-// Middleware
 const corsOptions = {
-    origin: 'https://client-pink-chi.vercel.app', // Add your frontend URL here
+    origin: 'http://localhost:5173', 
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   };
   app.use(cors(corsOptions));
   app.use(express.json({ extended: false }));
 
-// Define routes
 app.use('/api/auth', require('./routes/auth'));
 
-// Port
 const PORT = process.env.PORT || 5000;
 
-// Start server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
